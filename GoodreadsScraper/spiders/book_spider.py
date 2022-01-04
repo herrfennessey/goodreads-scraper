27a1686ce111d3f43064abda25df97026db3e84a
Dave Fennessey <vazheel@gmail.com>
2022-01-04 09:28:48 +0100

    urls should be relative, not absolute in this use case

--- GoodreadsScraper/spiders/book_spider.py
+++ GoodreadsScraper/spiders/book_spider.py
@@ -2,6 +2,7 @@
 import re
 
 import scrapy
+from urllib.parse import urlsplit
 
 from ..items import BookItem, BookLoader
 
@@ -9,20 +10,22 @@ SIMILAR_EDITIONS_ISBN_REGEX = re.compile(r"editionInfo\\'>\\nisbn:\s(\d+)\\n<\\"
 SIMILAR_EDITIONS_ISBN_13_REGEX = re.compile(r"editionInfo\\'>\\nisbn13:\s(\d+)\\n<\\")
 SIMILAR_EDITIONS_ASIN_REGEX = re.compile(r"editionInfo\\'>\\nasin:\s([A-Za-z0-9]+)\\n<\\")
 
+
 class BookSpider(scrapy.Spider):
     """Extract information from a /book/show type page on Goodreads"""
     name = "book"
 
     def __init__(self, books=None):
         super().__init__()
-        self.start_urls = books.split(",")
+        # self.start_urls = books.split(",")
         # For debugging
-        # self.start_urls = ["https://www.goodreads.com/book/show/5907.The_Hobbit_or_There_and_Back_Again"]
+        self.start_urls = ["https://www.goodreads.com/book/show/5907.The_Hobbit_or_There_and_Back_Again"]
 
     def parse(self, response):
         loader = BookLoader(BookItem(), response=response)
 
-        loader.add_value('url', response.request.url)
+        # I use relative paths because that's what's in the reviews
+        loader.add_value('url', urlsplit(response.request.url).path)
 
         loader.add_css("title", "#bookTitle::text")
         loader.add_css("author", "a.authorName>span::text")
@@ -54,8 +57,6 @@ class BookSpider(scrapy.Spider):
         loader.add_css('isbn13', 'div.infoBoxRowItem::text')
         loader.add_css('isbn13', 'script::text', re=SIMILAR_EDITIONS_ISBN_13_REGEX)
 
-
         loader.add_css('rating_histogram', 'script[type*="protovis"]::text')
 
         return loader.load_item()
-
